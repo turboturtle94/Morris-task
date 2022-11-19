0c1c571db59481ce6566286c9c2c0d8952de4407
turboturtle94 <hkashyap2894@gmail.com>
2022-11-19 23:53:29 +0530

    Initial commit of landing page
    Initial commit of landing page

---
+++ js/animate.js
@@ -0,0 +1,39 @@
+animateHeader();
+animateHeaderButtons();
+$(window).scroll(function () {
+  animateRows();
+});
+
+function isScrolledIntoView($elem) {
+  let docViewTop = $(window).scrollTop();
+  let docViewBottom = docViewTop + $(window).height();
+  let elemTop = $elem.offset().top;
+  let elemBottom = elemTop + $elem.height();
+  return elemBottom <= docViewBottom || elemTop >= docViewTop;
+}
+function animateRows(){
+  let grid = $(".section-about")[0];
+  if (isScrolledIntoView($(grid))) {
+    let gridCards = $(".card-initial");
+    let i = 0;
+    for (const element of gridCards) {
+      i += 100;
+      setTimeout(() => {
+        element.classList.remove("card-initial");
+        element.classList.add("card-animated");
+      }, i);
+    }
+  }
+};
+function animateHeader(){
+    let header = $(".heading-text-initial")[0];
+    $(header).removeClass("heading-text-initial");
+    $(header).addClass("heading-text-animated");
+}
+
+function animateHeaderButtons(){
+    debugger;
+    let buttonsDiv = $(".actions-wrapper-initial")[0];
+    $(buttonsDiv).removeClass("actions-wrapper-initial");
+    $(buttonsDiv).addClass("actions-wrapper-animated");
+}
